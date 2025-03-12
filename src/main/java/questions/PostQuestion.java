fb3f430effd70265fd9593e85226004af1f79dde
JuanPablo <juanpablo2543@gmail.com>
2025-03-12 01:29:40 -0500

    Se actualiza la clase PostQuestion y se agrega la validacion

--- src/main/java/questions/PostQuestion.java
+++ src/main/java/questions/PostQuestion.java
@@ -1,4 +1,17 @@
 package questions;
 
-public class PostQuestion {
+import net.serenitybdd.rest.SerenityRest;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+public class PostQuestion implements Question <Integer>  {
+
+    @Override
+    public Integer answeredBy(Actor actor) {
+        return SerenityRest.lastResponse().statusCode();
+    }
+
+    public static PostQuestion postQuestion(){
+        return new PostQuestion();
+    }
 }
